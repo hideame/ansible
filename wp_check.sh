e2b25467f401bdcf3e461cbb287c2d1c3a4ce911
Hideo <hamezawa3@gmail.com>
2019-07-07 12:38:53 +0900

    Update wp_check.sh

--- wp_check.sh
+++ wp_check.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
  
-echo "\n### wp --info ###"
+echo -e "\n### wp --info ###"
 wp --info
 
-echo "\n### theme status ###"
+echo -e "\n### theme status ###"
 wp theme status --path=/var/www/wordpress
 wp theme list --path=/var/www/wordpress
 
-echo "\n### plugin status ###"
+echo -e "\n### plugin status ###"
 wp plugin status --path=/var/www/wordpress
 wp plugin list --path=/var/www/wordpress
